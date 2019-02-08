d7aa37ddff35a7278cc4e9c34e2fcf4f50ad54be
alexpaul <alexpaulnyc@gmail.com>
2019-02-08 08:34:56 -0500

    added inline comments

--- PlistDemo/ViewController.swift
+++ PlistDemo/ViewController.swift
@@ -23,19 +23,27 @@ class ViewController: UIViewController {
           let coordStringArray = coordStrings.map { $0.components(separatedBy: ",") }
           let coordinates = coordStringArray.map { CLLocationCoordinate2DMake(Double($0[0])!, Double($0[1])!) }
           print(coordinates)
+          /*
+           [__C.CLLocationCoordinate2D(latitude: 40.7429595, longitude: -73.9441155),
+           __C.CLLocationCoordinate2D(latitude: 40.7457885, longitude: -73.8489019)]
+          */
         }
         if let pursuitDict = dict["Pursuit"] as? [String: AnyObject] {
           if let name = pursuitDict["Name"] as? String {
-            print(name)
+            print(name) // Pursuit
           }
           if let lat = pursuitDict["Latitude"] as? Double {
-            print(lat)
+            print(lat) // 40.7429595
           }
           if let lon = pursuitDict["Longitude"] as? Double {
-            print(lon)
+            print(lon) // -73.9441155
           }
           if let description = pursuitDict["Description"] as? String {
             print(description)
+            /*
+             Pursuit trains adults with the most need and potential to get their first tech jobs, advance in their careers,
+             and become the next generation of leaders in tech.
+            */
           }
         }
       }
